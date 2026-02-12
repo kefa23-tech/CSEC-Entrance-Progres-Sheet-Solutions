94619c71e9199aaf548b93924459d6cd66582641
kefa23-tech <wendimantru@gmail.com>
2026-02-12 13:24:18 +0300

    Add matrix input and find position of 1

---
+++ beautiful matrix.py
@@ -0,0 +1,11 @@
+matrix = []
+for i in range(5):
+    row = list(map(int,input().split()))
+    matrix.append(row)
+for i in range(5):
+    for j in range(5):
+        if matrix[i][j] == 1:
+            row = abs(2 - i)
+            column =abs( 2 - j)
+            print(row+column)
+            break
